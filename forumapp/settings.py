e19d5460e29ec7a61d757232b68fabe2e7414da0
anjlapastora <angelicalapastora@gmail.com>
2021-08-16 22:56:49 +0800

    Add prod ready config

--- forumapp/settings.py
+++ forumapp/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-l(^-yy$o@%imk9%$6wy$&zjo&k@4o2d1+9ib9ui*)bfi2_fi6d'
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['forumapp.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -174,3 +175,11 @@ WEBPACK_LOADER = {
         'STATS_FILE': BASE_DIR / 'frontend' / 'webpack-stats.json'
     }
 }
+
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+
+STATICFILES_DIRS = [
+  os.path.join(BASE_DIR, 'dist/static'),
+]
--- forumapp/wsgi.py
+++ forumapp/wsgi.py
@@ -1,16 +1,8 @@
-"""
-WSGI config for forumapp project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
-"""
-
 import os
-
 from django.core.wsgi import get_wsgi_application
+from whitenoise.django import DjangoWhiteNoise
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'forumapp.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "django_vue_template.settings")
 
 application = get_wsgi_application()
+application = DjangoWhiteNoise(application)
